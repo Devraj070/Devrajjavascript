5620053e10dc734a973a0df31c3693426ce718f1
Devraj070 <152358894+Devraj070@users.noreply.github.com>
2023-11-29 04:22:12 +0000

    file added

---
+++ 01-Basic/test.js
@@ -0,0 +1,10 @@
+console.log("hello world!");
+/*
+
+1. Java Script is a High level Scripting language used on developing web and mobile applications efficiently.
+2. we use javaSCript in  both client side and serverside .
+it was developed in 1997
+
+3. Nowdays we used ES6 version of javascript it was started on 2015 and now it is the latest version of us.
+
+*/
