0c68f1fd37cb5d94c418f097e266484a510a411f
Devraj070 <152358894+Devraj070@users.noreply.github.com>
2023-12-04 04:20:14 +0000

    file added

---
+++ 01-Basic/04-String.js
@@ -0,0 +1,20 @@
+const name = "Devraj";
+console.log(name);
+const count = 50;
+
+console.log(name+count);
+
+console.log(`my name is ${name}`);
+
+const name2 = new String("DeV");
+console.log(name);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.indexOf("r"));
+console.log(name.charAt(3));
+console.log(name.);
+
+
+
+
+
