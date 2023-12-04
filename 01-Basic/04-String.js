e78b6a850b8f7516834771a7a881ffe0473f20e2
Devraj070 <152358894+Devraj070@users.noreply.github.com>
2023-12-04 05:06:55 +0000

    file added

--- 01-Basic/04-String.js
+++ 01-Basic/04-String.js
@@ -12,9 +12,20 @@ console.log(name.toUpperCase());
 console.log(name.toLowerCase());
 console.log(name.indexOf("r"));
 console.log(name.charAt(3));
-console.log(name.);
 
+const newName = name.substring(0,3);
+console.log(newName);
 
+const myCollegeName= "                                               gift  autonomous college";
+console.log(myCollegeName);
+console.log(myCollegeName.trim());
 
+const anotherName = name.slice(0,3); //slic()  -  returns the selected elements.
+console.log(anotherName);
+const url = "www.webbocket.com";
+console.log(url.replace("com","in"));
+
+const demo = "gift-is-a-bad-college";
+console.log(demo.split('-'));
 
 
---
+++ 01-Basic/05-number-math.js
@@ -0,0 +1,16 @@
+const score = 400;
+console.log(score);
+
+const newScore = new Number(100);
+console.log(newScore);
+
+console.log(newScore.toString().length);
+
+console.log(newScore.toFixed(2));
+
+const otherNumber = 125.8120;
+console.log(otherNumber.toPrecision(4));
+
+
+const demoNumber = 1000000;
+console.log(demoNumber.toLocaleString());
